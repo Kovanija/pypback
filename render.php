d5f0aa905deed1a5ecac7caffac40eb32fb0a398
Ubuntu <ubuntu@ip-172-31-47-117.eu-central-1.compute.internal>
2019-07-30 08:52:45 +0000

    fix

--- render.php
+++ render.php
@@ -9,12 +9,10 @@ switch($template_id)
 {
 
 case 1:
-exec("blender -b /templates/LogoRevealPlexus/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/test2.mp4 -ird /var/www/html/test 2>&1 &", $output);
-echo json_encode(array('error' => $output));
+exec("blender -b /templates/LogoRevealPlexus/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/test2.mp4  2>&1 &");
 break;
 case 11: 
-exec("blender -b /templates/SFLogoReveal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py 2>&1 &", $output);
-echo json_encode(array('error' => $output)); 
+exec("blender -b /templates/SFLogoReveal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py 2>&1 &");
 break;
 
 }
