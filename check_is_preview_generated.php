b31d144ff8bee7b3eae6e698ad9c5939af98a6ac
Ubuntu <ubuntu@ip-172-31-47-117.eu-central-1.compute.internal>
2019-07-30 09:30:21 +0000

    upgrades

--- check_is_preview_generated.php
+++ check_is_preview_generated.php
@@ -1,14 +1,14 @@
 <?php
-
+header("Access-Control-Allow-Origin: *");
 
 $proj_id = $_GET['project_id'];
 
 $filename = '/var/www/html/prodid_'.$proj_id.'.mp4';
 
 if (file_exists($filename)) {
-    echo json_encode('is_exists' => true);
+    echo json_encode(array('is_exists' => true));
 } else {
-    echo json_encode('is_exists' => false);
+    echo json_encode(array('is_exists' => false));
 }
 
 ?>
