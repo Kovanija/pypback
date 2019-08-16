d50139918455ce0db790aa480f496651853fa513
Ubuntu <ubuntu@ip-172-31-47-117.eu-central-1.compute.internal>
2019-08-16 13:16:20 +0000

    ird applied

--- render.php
+++ render.php
@@ -7,18 +7,17 @@ $template_id = $_POST['template_id'];
 
 switch($template_id)
 {
-
 case 1:
-exec("blender -b /templates/LogoRevealPlexus/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
+exec("blender -b /templates/LogoRevealPlexus/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 -ird /var/www/html/test/ 2>&1 &", $output);
+file_put_contents('/var/www/html/output.txt', $output);
 break;
 case 11: 
 exec("blender -b /templates/SFLogoReveal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
 break;
 case 21:
-exec("blender -b /templates/NGPersonal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
+exec("blender -b /templates/NGGreetengsPersonal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
 break;
 case 31:
-exec("blender -b /templates/NGLogo/standard.360p.blend -P /html/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
+exec("blender -b /templates/NGGreetengsLogo/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
 break;
 }
-
