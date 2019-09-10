9157a162cf3882d6671c85d27e09843d99f29446
Ubuntu <ubuntu@ip-172-31-47-117.eu-central-1.compute.internal>
2019-09-10 20:25:29 +0000

    fix

--- check_is_preview_generated.php
+++ check_is_preview_generated.php
@@ -5,10 +5,7 @@ $proj_id = $_GET['project_id'];
 
 $filename = '/var/www/html/prodid_'.$proj_id.'.mp4';
 
-if (file_exists($filename)) {
-    echo json_encode(array('is_exists' => true));
-} else {
-    echo json_encode(array('is_exists' => false));
-}
+echo json_encode(array('is_exists' => file(exists($filename))));
+   
 
 ?>
--- render.php
+++ render.php
@@ -8,16 +8,18 @@ $template_id = $_POST['template_id'];
 switch($template_id)
 {
 case 1:
-exec("blender -b /templates/LogoRevealPlexus/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 -ird /var/www/html/test/ 2>&1 &", $output);
-file_put_contents('/var/www/html/output.txt', $output);
+exec("blender -b /templates/LogoRevealPlexus/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 -ird /var/www/html/test2 > /dev/null 2>&1 &", $output);
+//file_put_contents('/var/www/html/output.txt', $output);
 break;
 case 11: 
-exec("blender -b /templates/SFLogoReveal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
+exec("blender -b /templates/SFLogoReveal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 -ird /var/www/html/test > /dev/null 2>&1 &", $output);
+//file_put_contents('/var/www/html/output.txt', $output);
 break;
 case 21:
-exec("blender -b /templates/NGGreetengsPersonal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
+exec("blender -b /templates/NGGreetengsPersonal/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 -ird /var/www/html/test2 > /dev/null 2>&1 &");
 break;
 case 31:
-exec("blender -b /templates/NGGreetengsLogo/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -so '$scene' -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &");
+exec("blender -b /templates/test/NGGreetengsLogoAlembicIzmenjenaPutanja/standard.360p.blend -P /home/ubuntu/blenderer/blenderer.py -- -ro /var/www/html/prodid_".$proj_id.".mp4 > /dev/null 2>&1 &", $output);
+//file_put_contents('/var/www/html/output.txt', $output);
 break;
 }
